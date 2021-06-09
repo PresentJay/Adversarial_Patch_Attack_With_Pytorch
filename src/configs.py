6040317434c3162d81008cf81345ac7592b7aeb4
presentj94 <presentj94@gmail.com>
2021-06-09 17:28:46 +0900

    chore: modify configs

--- src/configs.py
+++ src/configs.py
@@ -25,7 +25,6 @@ def Initialization():
     parser.add_argument('--batch_size', type=int, default=20, help="batch size")
     
     # about Training Adversarial Patch
-    parser.add_argument('--noise_percentage', type=float, default=0.1, help="percentage of the patch size compared with the image size")
     parser.add_argument('--max_iteration', type=int, default=1000, help="max number of iterations to find adversarial example")
     parser.add_argument('--patch_size', type=float, default=0.5, help='patch size. E.g. 0.05 ~= 5% of image ')
     parser.add_argument('--lr', type=float, default=1.0, help="learning rate")
@@ -33,7 +32,7 @@ def Initialization():
 
     # about Adversarial Patch Condition
     parser.add_argument('--target', type=int, default=859, help="The target class index: 859 == toaster")
-    parser.add_argument('--patch_type', type=str, default='circle', help="type of the patch => (circle or rectangle)")
+    parser.add_argument('--patch_type', type=str, default='square', help="type of the patch => (circle or square)")
     parser.add_argument('--probability_threshold', type=float, default=0.9, help="Stop attack on image when target classifier reaches this value for target class")
     
     # about logging
