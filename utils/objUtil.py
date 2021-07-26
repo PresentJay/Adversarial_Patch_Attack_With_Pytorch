220ea813a493c078a392ab215c3c318264ef1134
presentj94 <presentj94@gmail.com>
2021-07-26 17:01:42 +0900

    feat: add get_errored_objs

--- utils/objUtil.py
+++ utils/objUtil.py
@@ -1,16 +1,15 @@
-from os import listdir
-import os
+from os import listdir, path
 import pickle
 
 def get_obj_dirs():
-    current_dir = os.path.abspath(os.path.curdir)
-    obj_dir=os.path.join(current_dir,"results")
+    current_dir = path.abspath(path.curdir)
+    obj_dir=path.join(current_dir,"results")
     dirs = []
     for experiment in listdir(obj_dir):
-        candidate_dir = os.path.join(obj_dir, experiment)
-        if os.path.isdir(candidate_dir):
+        candidate_dir = path.join(obj_dir, experiment)
+        if path.isdir(candidate_dir):
             for timestamp in listdir(candidate_dir):
-                valid_dir = os.path.join(candidate_dir, timestamp)
+                valid_dir = path.join(candidate_dir, timestamp)
                 for filedir in listdir(valid_dir):
                     if filedir.split('.')[1] == 'pkl':
                         dirs.append(valid_dir)
@@ -20,3 +19,15 @@ def get_obj_dirs():
     
 def update_from_log():
     pass
+
+
+def get_errored_objs():
+    errored_list = []
+    targets = get_obj_dirs()
+    for target in targets:
+        for file in listdir(target):
+            if file == "patch(got_errored).pkl":
+                errored_list.append(path.join(target, file))
+    return errored_list
+
+print(get_errored_objs())
